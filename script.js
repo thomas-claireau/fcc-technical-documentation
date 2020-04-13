c39b900632aba30af247e2df654da3cc55a2a4e8
Thomas Claireau <thomas.claireau@gmail.com>
2020-04-13 15:37:21 +0200

    add script for generate nav + scroll anim

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-window.addEventListener('DOMContentLoaded', (event) => {});
+window.addEventListener('DOMContentLoaded', (event) => {
+	const navbar = document.querySelector('#navbar');
+	const mainSections = document.querySelectorAll('.main-section');
+
+	if (!navbar || !mainSections) return;
+
+	mainSections.forEach((section) => {
+		// navbar auto
+		const id = section.getAttribute('id');
+
+		if (!id) return;
+
+		const viewId = id.replace(/_/g, ' ');
+
+		if (!viewId) return;
+
+		const link = document.createElement('a');
+		link.classList.add('nav-link');
+		link.textContent = viewId;
+		link.href = '#' + id;
+
+		navbar.appendChild(link);
+	});
+
+	const links = navbar.querySelectorAll('.nav-link');
+
+	links.forEach((link) => {
+		// scroll animate
+		link.addEventListener('click', (e) => {
+			e.preventDefault();
+			let href = link.href;
+
+			href = href.split('#')[1];
+
+			const target = document.getElementById(href);
+
+			if (!target) return;
+
+			target.scrollIntoView({ behavior: 'smooth' });
+		});
+	});
+});
